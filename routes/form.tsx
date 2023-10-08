a329ec46a9a983b81252ceb2029de572f00eb9be
Cyanne Ramnarine <55121041+ccramnar@users.noreply.github.com>
2023-10-08 15:56:31 -0400

    form

---
+++ routes/form.tsx
@@ -0,0 +1,109 @@
+export default function Form() {
+  return (
+    <form>
+      <div className="space-y-12 sm:space-y-16 p-4">
+        <div>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Tribute Message
+          </h2>
+          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
+            Share Your Tribute to Curtis: We invite you to submit a heartfelt
+            message or memory in honor of Curtis. Let your words be a part of
+            the enduring tribute to his life and legacy.
+          </p>
+
+          <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
+            <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+              >
+                From
+              </label>
+              <div className="mt-2 sm:col-span-2 sm:mt-0">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
+                  </span>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Your name"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+              <label
+                htmlFor="about"
+                className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+              >
+                Message
+              </label>
+              <div className="mt-2 sm:col-span-2 sm:mt-0">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full max-w-2xl rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={""}
+                />
+              </div>
+            </div>
+
+            <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+              <label
+                htmlFor="cover-photo"
+                className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+              >
+                Photo
+              </label>
+              <div className="mt-2 sm:col-span-2 sm:mt-0">
+                <div className="flex max-w-2xl justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                  <div className="text-center">
+                    <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                      <label
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer rounded-md bg-white font-semibold text-yellow-400 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                      >
+                        <span>Upload a Photo</span>
+                        <input
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          className="sr-only"
+                        />
+                      </label>
+                      <p className="pl-1">or drag and drop</p>
+                    </div>
+                    <p className="text-xs leading-5 text-gray-600">
+                      PNG, JPG, GIF up to 10MB
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="inline-flex justify-center rounded-md bg-yellow-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Post
+        </button>
+      </div>
+    </form>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,14 +1,48 @@
 import { useSignal } from "@preact/signals";
+import Form from "./form.tsx";
+
+const text =
+  "Curtis was, without a doubt, the life of the party. He had an incredible zest for life and an enthusiasm that was contagious. He knew how to have a good time and how to make sure everyone else did too. His laughter echoed through the best moments of our lives, and we will forever cherish the joy he brought us. /n Beyond his creative talents and his ability to bring joy to any occasion, Curtis was a devoted family man. He cherished his loved ones and held them close to his heart. His family was the center of his universe, and he never missed an opportunity to express his love and gratitude for them. Curtis was also a logical thinker, a quality that served him well in his creative endeavors. He approached his work with precision and attention to detail, ensuring that every decoration and every event was a masterpiece. As we say our final goodbyes to Curtis, let us remember the legacy he leaves behind. A legacy of creativity, friendship, family, logic, and, above all, a legacy of being the life of the party. He brought us together, made our lives more beautiful, and filled our hearts with happiness. Though Curtis may no longer be with us in person, his spirit lives on in the memories we hold dear. Let us celebrate his life, his creativity, his kindness, and his infectious spirit. Curtis Samlal will forever remain in our hearts, a shining star in the tapestry of our lives.";
 
 export default function Home() {
   return (
-    <div class="grid min-h-screen grid-cols-12">
-      <div class="col-span-4 min-h-screen bg-gray-100">
-        <img src="logo.svg" alt="logo" />
+    <div class="bg-white">
+      <div class="mx-auto max-w-7xl py-10  sm:py-10 lg:px-4">
+        <div class="mx-auto max-w-2xl px-4 lg:max-w-none">
+          <div class="max-w-3xl">
+            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Remembering Curtis Samlal
+            </p>
+          </div>
+
+          <div class="mt-1 space-y-16 border-t border-gray-200 pt-1 sm:mt-5 sm:pt-5">
+            <div class="flex flex-col-reverse lg:grid lg:grid-cols-12 lg:items-center lg:gap-x-8">
+              <div class="flex-auto lg:col-span-5 xl:col-span-6">
+                <div class="aspect-h-2 aspect-w-6 overflow-hidden rounded-lg bg-gray-100">
+                  <img
+                    src="landing_pic.jpg"
+                    alt="Printed photo of bag being tossed into the sky on top of grass."
+                    class="object-cover object-center"
+                  />
+                </div>
+              </div>
+              <div class="mt-6 lg:col-span-6 lg:mt-0 xl:col-span-6">
+                <h3 class="text-lg font-medium text-gray-900">
+                  Curtis Samlal
+                </h3>
+                <p class="mt-2 text-sm text-gray-500">
+                  {text}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="col-span-8 min-h-screen bg-gray-200">this is placeholder</div>
+
       <div class="col-span-12 min-h-screen flex justify-center items-center">
-        <div class="p-4 rounded bg-gray-200">this is more content</div>
+        <div class="p-4 rounded bg-white">
+          <Form />
+        </div>
       </div>
     </div>
   );
