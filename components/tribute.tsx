a116f42c73392c93b0a7a22715f4b952cc4855a1
Cyanne Ramnarine <55121041+ccramnar@users.noreply.github.com>
2023-10-09 14:49:08 -0400

    stuuf

--- components/tribute.tsx
+++ components/tribute.tsx
@@ -7,18 +7,20 @@ export default function Tribute(props: { message: MessageOut }) {
       <div class="flex flex-shrink-0 p-4 pb-0">
         <a href="#" class="flex-shrink-0 group block">
           <div class="flex items-center">
-            <p class="text-base leading-6 font-medium text-black">{from}</p>
+            <p class="text-base leading-6 font-medium text-yellow-700 uppercase">
+              {from}
+            </p>
           </div>
         </a>
       </div>
-      <div class="flex flex-shrink-0 p-4 pb-4">
-        <p class="text-base width-auto font-medium text-black flex-shrink">
+      <div class="flex-col gap-x-4 flex-shrink-0 p-4 pb-4">
+        <p class="text-base width-auto font-medium text-black flex-shrink pb-2">
           {text}
         </p>
         {props.message.imagePointer && (
           <img
             src={props.message.imagePointer}
-            class="w-20 h-20 object-cover"
+            class="w-full h-24 object-contain"
           />
         )}
       </div>
