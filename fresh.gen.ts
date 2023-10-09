7bd13f5ae52d5f8f994e018d75f601819401c1fa
Cyanne Ramnarine <55121041+ccramnar@users.noreply.github.com>
2023-10-08 23:37:27 -0400

    update imports

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,8 +7,7 @@ import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/index.ts";
 import * as $3 from "./routes/form.tsx";
 import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/tribute.tsx";
-import * as $6 from "./routes/tributes.tsx";
+import * as $5 from "./routes/tributes.tsx";
 
 const manifest = {
   routes: {
@@ -17,8 +16,7 @@ const manifest = {
     "./routes/api/index.ts": $2,
     "./routes/form.tsx": $3,
     "./routes/index.tsx": $4,
-    "./routes/tribute.tsx": $5,
-    "./routes/tributes.tsx": $6,
+    "./routes/tributes.tsx": $5,
   },
   islands: {},
   baseUrl: import.meta.url,
--- routes/tributes.tsx
+++ routes/tributes.tsx
@@ -1,10 +1,10 @@
 import Tribute from "../components/tribute.tsx";
 import { MessageOut } from "./api/index.ts";
 
-export default function Tributes(tributes: any[]) {
+export default function Tributes(props: { tributes: any[] }) {
   return (
-    <div>
-      {tributes.map(function (d) {
+    <div class="w-3/5 border border-gray-600 h-auto  border-t-0">
+      {props.tributes.map(function (d) {
         return <Tribute props={d}></Tribute>;
       })}
     </div>
