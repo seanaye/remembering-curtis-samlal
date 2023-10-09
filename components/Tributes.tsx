cf12323728ef5a4ffe8421d467db826191581966
Sean Aye <hello@seanaye.ca>
2023-10-09 15:07:24 -0400

    fix grid spacing

--- components/Tributes.tsx
+++ components/Tributes.tsx
@@ -4,7 +4,7 @@ import { MessageOut } from "../routes/api/index.ts";
 export default function Tributes(props: { tributes: Array<MessageOut> }) {
   return (
     <div class="col-span-12 min-h-screen px-6" id="tributes">
-      <div class="grid grid-flow-row gap-6 grid-cols-6 items-start">
+      <div class="grid grid-flow-row auto-rows-max gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 items-start">
         {props.tributes.map(function (d) {
           return <Tribute message={d} />;
         })}
